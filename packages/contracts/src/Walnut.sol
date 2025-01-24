845e0cc8d15f8b5cb87820b56b629be4b24f595f
Ameya Deshmukh <ad@seismic.systems>
2025-01-23 20:24:29 -0500

    initial contract for signed reads+round-based

--- packages/contracts/src/Walnut.sol
+++ packages/contracts/src/Walnut.sol
@@ -4,13 +4,17 @@ pragma solidity ^0.8.13;
 contract Walnut {
     uint256 startShell;
     uint256 shell;
+    uint256 round;
 
     suint256 startNumber;
     suint256 number;
 
-    event Hit(uint256 remaining);
-    event Shake();
-    event Reset(uint256 remaining);
+    mapping(uint256 => mapping (address => uint256)) hitsPerRound;
+
+    
+    event Hit(uint256 indexed round, address indexed hitter, uint256 remaining);
+    event Shake(uint256 indexed round, address indexed shaker);
+    event Reset(uint256 indexed newRound, uint256 remaining);
 
     constructor(uint256 _shell, suint256 _number) {
         startShell = _shell;
@@ -18,27 +22,35 @@ contract Walnut {
 
         startNumber = _number;
         number = _number;
+
+        round = 1;
     }
 
     function hit() public {
-        if (shell > 0) shell--;
-        emit Hit(shell);
+        require(shell > 0, "SHELL_ALREADY_CRACKED");
+        shell--;
+        hitsPerRound[round][msg.sender]++;
+        emit Hit(round, msg.sender, shell);
     }
 
     function shake() public {
         number++;
-        emit Shake();
+        emit Shake(round, msg.sender);
     }
+
 
     function reset() public {
         shell = startShell;
         number = startNumber;
-        emit Reset(shell);
+        round++; // Move to the next round
+        emit Reset(round, shell);
     }
 
-    function look() public view requireCracked returns (uint256) {
+
+     function look() public view requireCracked onlyContributor returns (uint256) {
         return uint256(number);
     }
+
 
     function set_number(suint _number) public {
         number = _number;
@@ -46,6 +58,11 @@ contract Walnut {
 
     modifier requireCracked() {
         if (shell > 0) revert("SHELL_INTACT");
+        _;
+    }
+
+    modifier onlyContributor() {
+        require(hitsPerRound[round][msg.sender] > 0, "NOT_A_CONTRIBUTOR");
         _;
     }
 }
